cbc9aa23007ddba9e61e72dc04b3a8e16b85fa8d
Victor Andrade <victor.ramos.andrade@hotmail.com>
2026-07-06 12:26:39 +0100

    Prepare main.py with test functions

--- main.py
+++ main.py
@@ -1,2 +1,43 @@
+import data
 
 class TestUrbanRoutes:
+
+    def test_set_route(self):
+        # Adicionar em S8
+        print("função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        print("função criada para selecionar o plano")
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        print("função criada para preencher o número de telefone")
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        print("função criada para preencher o cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        print("função criada para adicionar comentário para o motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        print("função criada para pedir cobertor e lenços")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Adicionar em S8
+        print("função criada para pedir 2 sorvetes")
+        pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        print("função criada para verificar se o modelo do carro aparece")
+        pass
